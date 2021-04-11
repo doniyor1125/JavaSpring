d7c98f81998ab43083c74727d62b1f8e7b3a3d98
doniyor1125 <79887075+doniyor1125@users.noreply.github.com>
2021-04-11 14:03:37 +0500

    Delete books directory

--- books/src/main/java/com/doniyor/BooksRepository.java
+++
@@ -1,8 +0,0 @@
-package com.doniyor;
-
-import com.doniyor.model.Books;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BooksRepository extends CrudRepository<Books, Integer>
-{
-}
